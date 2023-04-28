87fb9c206176add258e0399c3bcc9f4d760afe32
Virgil <949508510@qq.com>
2023-04-28 21:00:54 +0800

    finish: server

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, send_file, request
 
 app = Flask(__name__, template_folder='static/template', static_folder='static')
 
 
 @app.route('/', methods=['GET'])
 def Home():
-    return render_template('home.html')
+    return send_file('static/template/index.html')
 
 
-@app.route('/gdp', methods=['GET'])
+@app.route('/GDP', methods=['GET'])
 def get_GDP():
     return render_template('GDP.html')
 
 
-@app.route('/map', methods=['GET'])
+@app.route('/AlongYear', methods=['GET'])
 def get_AlongYear():
-    return render_template('AlongYear.html')
+    year = request.args.get('year')
+    return render_template('AlongYear.html', year=year)
 
 
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=80)
-    app.run()
+    app.run(port=8080)
